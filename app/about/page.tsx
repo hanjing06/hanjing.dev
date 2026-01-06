fb057c90135b17b377c017791d350fbd69909d50
hanjing06 <hanjinglin8@gmail.com>
2026-01-05 23:25:28 -0500

    in tutorial hell currently

---
+++ app/about/page.tsx
@@ -0,0 +1,30 @@
+"create client";
+
+const time = [{
+    title: "background",
+    descriptions: [{
+        description: "i did freelance video editing for all of covid",
+    },{ description: "my bench pr was 115lbs"   
+
+    }]
+},
+
+{   title: "now",
+    descriptions: [{
+        description:"second year at western"
+    },{ description:"??"
+
+    }],
+    tags: ["SE2203", "SE2250"]
+    
+}, 
+
+{   title: "next",
+    description: "incoming @"
+}]
+
+export default function About(){
+    return (
+        <p>my life up until now</p>
+    )
+}
