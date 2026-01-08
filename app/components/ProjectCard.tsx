fd26cbf4ea1097f38ea6e2ff2a2034e75fb2c59c
hanjing06 <hanjinglin8@gmail.com>
2026-01-08 00:21:26 -0500

    Added title, description and listed unique tags

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,9 +1,10 @@
 "use client";
+import About from "../components/page";
 
 interface ProjectCard{
     title: string;
     description: string;
-    tags: string;
+    tags: string[];
     logo: string;
     link: string;
     demoVideo: string;
@@ -11,12 +12,22 @@ interface ProjectCard{
     date: string;
 }
 
-export default function ProjectCard(card: ProjectCard){
-    return ( <div className="">
+export default function ProjectCard({title, description, tags, logo, link, demoVideo, insideDesc, date}: ProjectCard){
+    return ( 
+    <section>
+        <div className="card m-10 rounded-xl bg-gray-50 place-content-center h-60">
             <img></img>
-            <h2>{card.title}</h2>
-            <p>Projects</p>
+            <div className ="text-center">{title}</div>
+            <div className ="text-center">{description}</div>
+            <div className = "flex-row flex gap-2 place-content-center">
+                {tags.map((tag, index) => {
+                    return(
+                    <span key = {index} className = "bg-gray-100 rounded-xl w-30 text-center">
+                        {tag}
+                    </span>
+                )})}
+            </div>
         </div>
-       
+    </section>
     );
 };
