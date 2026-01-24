44ad4aeec1ae13a70d59af07d52c06c6f76614e3
hanjing06 <hanjinglin8@gmail.com>
2026-01-23 23:17:21 -0500

    Added css to coomponent

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -4,6 +4,7 @@ interface ProjectCard{
     title: string;
     description: string;
     tags: string[];
+    image: string;
     logo: string;
     link: string;
     demoVideo: string;
@@ -11,25 +12,30 @@ interface ProjectCard{
     date: string;
 }
 
-export default function ProjectCard({title, description, tags, logo, link, demoVideo, insideDesc, date}: ProjectCard){
+export default function ProjectCard({title, description, tags, image, logo, link, demoVideo, insideDesc, date}: ProjectCard){
     return ( 
     <section>
-        <div className="rounded-xl bg-blue-500 flex items-center justify-center w-100 h-100">
-            <div className="flex flex-col items-center text-center gap-2">
-                <div>{title}</div>
-                <div>{description}</div>
+        <a href={link} target="loading">
+            <div className="grid grid-3 overflow-hidden rounded-xl bg-white shadow-md items-center justify-center h-100 md:w-xs hover:translate-y-[-4px] duration-300 ease-in-out">
+                <img src={image} className="h-full w-xl rounded-t-lg"/>
+                <div className="h-50 flex-col items-center text-center">
+                    <div className="h-10 items-center justify-center w-full line-clamp-2">{title}</div>
+                    <div className="h-10 overflow-hidden w-full line-clamp-3">{description}</div>
 
-                <div className = "flex flex-wrap gap-2 justify-center flex">
-                    {tags.map((tag, index) => {
-                        return(
-                        <span key = {index} className = "bg-gray-100 rounded-xl w-30 text-center">
-                            {tag}
-                        </span>
-                    )})}
+                    <div className = "h-20 w-full overflow-y-auto">
+                        <div className="flex flex-wrap gap-2 justify-center">
+                        {tags.map((tag, index) => {
+                            return(
+                            <span key = {index} className = "bg-gray-100 rounded-xl w-30 text-center">
+                                {tag}
+                            </span>
+                        )})}
+                        </div>
+            
                 </div>
-
+                </div>
             </div>
-        </div>
+        </a>
     </section>
     );
 };
