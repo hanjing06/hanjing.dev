7ace908ef262a900f92df44653df2a9b8b670646
hanjing06 <hanjinglin8@gmail.com>
2026-01-23 21:29:16 -0500

    Added scroll to section

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Link from "next/link";
 
 export default function NavBar(){
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, sectionId: string) => {
+    e.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  };
     return (
     <div className="mt-8 sm:mt-18 flex justify-center items-center gap-6">
-        <Link href="/">home</Link>
-        <Link href="/projects">projects</Link>
-        <Link href="/about">about</Link>
+        <Link href="#" onClick={(e) => handleClick(e, 'home')}>home</Link>    
+        <Link href="#projects" onClick={(e) => handleClick(e, 'projects')}>projects</Link>
+        <Link href="#about" onClick={(e) => handleClick(e, 'about')}>about</Link>
     </div>
     );
 }
