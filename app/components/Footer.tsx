435a6e43cd16115f7335e9ffe68b8b962fa2f2d9
hanjing06 <hanjinglin8@gmail.com>
2026-03-11 20:44:35 -0400

    mailing

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="mx-auto max-w-[1100px] px-8 md:px-16 mb-20">
         <div className="my-4 h-px border-t-0 bg-black"></div>
         <hr className="h-px my-8 bg-neutral-quaternary border-0"></hr>
-        <div className="justify-start text-black text-xl mt-[30px]">hlin389@uwo.ca
+        <div className="justify-start text-black text-xl mt-[30px]"><a href="mailto:hlin389@uwo.ca">hlin389@uwo.ca</a>
         <div className="ml-[450px] text-right text-black text-xl inline">© 2026 Hanjing Lin. All rights reserved.</div>
         </div>
     </footer>
